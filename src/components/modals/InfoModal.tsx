936005758aef7f01edc6fee8e5651b3e43da3dc5
Objectivitix <itsallobjects@gmail.com>
2022-04-01 20:03:24 -0300

    Source code reveal :eyes:

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -8,63 +8,66 @@ type Props = {
 
 export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Welcome to Clashle!" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the Clash Royale card in 5 tries. After each guess, the color of the tiles will
+        change to show how close your guess was to the correct card name.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="B"
           status="correct"
         />
-        <Cell value="E" />
         <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
-      </div>
-      <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
-      </p>
-
-      <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
+        <Cell value="N" />
+        <Cell value="D" />
         <Cell value="I" />
-        <Cell
-          isRevealing={true}
-          isCompleted={true}
-          value="L"
-          status="present"
-        />
-        <Cell value="O" />
         <Cell value="T" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter B is in the name and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="R" />
+        <Cell value="R" />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="O"
+          status="present"
+        />
+        <Cell value="W" />
+        <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
+        The letter O is in the name but in the wrong spot.
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell value="T" />
+        <Cell value="H" />
+        <Cell value="E" />
+        <Cell isRevealing={true} isCompleted={true} value="L" status="absent" />
+        <Cell value="O" />
+        <Cell value="G" />
+      </div>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        The letter L is not in the word in any spot.
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is a Clash Royale version of the word guessing game we all know and
+        love, and it is open source -{' '}
         <a
-          href="https://github.com/cwackerfuss/react-wordle"
+          href="https://github.com/Objectivitix/Objectivitix.github.io"
           className="underline font-bold"
         >
-          check out the code here
+          check out the code here!
         </a>{' '}
       </p>
     </BaseModal>
